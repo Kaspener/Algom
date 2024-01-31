1c99002b177379a56903f6d49731bec6893519e3
Kaspener <daniil.shpilev@gmail.com>
2024-02-01 01:33:45 +0700

    Zhordan-Gauss

---
+++ fraction.cpp
@@ -0,0 +1,151 @@
+#include "fraction.hpp"
+#include <numeric>
+
+Fraction::Fraction(long long n, long long d)
+{
+    if (d == 0)
+    {
+        std::cerr << "Denominator may not be 0." << std::endl;
+        exit(0);
+    }
+    if (n == 0)
+    {
+        numerator = 0;
+        denominator = 1;
+    }
+    else
+    {
+        numerator = n;
+        denominator = d;
+        calculateGcd();
+    }
+}
+
+void Fraction::calculateGcd()
+{
+    int sign = 1;
+    if (numerator < 0)
+    {
+        sign *= -1;
+        numerator *= -1;
+    }
+    if (denominator < 0)
+    {
+        sign *= -1;
+        denominator *= -1;
+    }
+
+    long long tmp = std::gcd(numerator, denominator);
+    numerator = numerator / tmp * sign;
+    denominator = denominator / tmp;
+}
+
+Fraction Fraction::operator+(const Fraction &other) const
+{
+    return Fraction{numerator * other.denominator + other.numerator * denominator, denominator * other.denominator};
+}
+
+Fraction &Fraction::operator+=(const Fraction &other)
+{
+    Fraction tmp = Fraction{numerator * other.denominator + other.numerator * denominator, denominator * other.denominator};
+    *this = tmp;
+    return *this;
+}
+
+Fraction Fraction::operator-(const Fraction &other) const
+{
+    return Fraction{numerator * other.denominator + other.numerator * denominator, denominator * other.denominator};
+}
+
+Fraction Fraction::operator-() const{
+    return Fraction{-numerator, denominator};
+}
+
+Fraction &Fraction::operator-=(const Fraction &other)
+{
+    Fraction tmp = Fraction{numerator * other.denominator - other.numerator * denominator, denominator * other.denominator};
+    *this = tmp;
+    return *this;
+}
+
+Fraction Fraction::operator*(const Fraction &other) const
+{
+    return Fraction{numerator * other.numerator, denominator * other.denominator};
+}
+
+Fraction &Fraction::operator*=(const Fraction &other)
+{
+    Fraction tmp = Fraction{numerator * other.numerator, denominator * other.denominator};
+    *this = tmp;
+    return *this;
+}
+
+Fraction &Fraction::operator*=(const int &other)
+{
+    Fraction tmp = Fraction{numerator * other, denominator};
+    *this = tmp;
+    return *this;
+}
+
+Fraction Fraction::operator/(const Fraction &other) const
+{
+    return Fraction{numerator * other.denominator, denominator * other.numerator};
+}
+
+Fraction &Fraction::operator/=(const Fraction &other)
+{
+    Fraction tmp = Fraction{numerator * other.denominator, denominator * other.numerator};
+    *this = tmp;
+    return *this;
+}
+
+bool Fraction::operator==(const Fraction &other) const
+{
+    return numerator == other.numerator && denominator == other.denominator;
+}
+
+bool Fraction::operator<(const Fraction &other) const
+{
+    return numerator*other.denominator<denominator*other.numerator;
+}
+
+bool Fraction::operator>(const Fraction &other) const
+{
+    return numerator*other.denominator>denominator*other.numerator;
+}
+
+std::ostream &operator<<(std::ostream &os, const Fraction &dt)
+{
+    if (dt.denominator != 1)
+        os << dt.numerator << '/' << dt.denominator;
+    else
+        os << dt.numerator;
+    return os;
+}
+
+std::istream &operator>>(std::istream &is, Fraction &dt)
+{
+    std::string str;
+    Fraction tmp;
+    is >> str;
+    int pos = str.find('/');
+    if (pos == std::string::npos)
+    {
+        dt.numerator = std::stoll(str);
+    }
+    else
+    {
+        dt.numerator = std::stoll(str.substr(0, pos));
+        dt.denominator = std::stoll(str.substr(pos + 1));
+        dt.calculateGcd();
+    }
+    return is;
+}
+
+Fraction Fraction::abs(const Fraction &x)
+{
+    Fraction tmp = x;
+    if (tmp.numerator < 0)
+        tmp.numerator = -tmp.numerator;
+    return tmp;
+}
---
+++ main.cpp
@@ -0,0 +1,215 @@
+#include "fraction.hpp"
+#include <fstream>
+#include <string>
+#include <vector>
+
+class Matrix
+{
+private:
+    int n, m;
+    std::vector<std::vector<Fraction>> matrix;
+    void setMaxElement(int i, int j);
+    bool checkZero(int i, int j);
+    bool noSolutions();
+
+public:
+    Matrix(std::ifstream &in);
+
+    void printMatrix() const;
+    void JordanGauss();
+};
+
+Matrix::Matrix(std::ifstream &in)
+{
+    in >> n >> m;
+    matrix.resize(n, std::vector<Fraction>(m));
+    for (auto &it : matrix)
+    {
+        for (auto &num : it)
+        {
+            in >> num;
+        }
+    }
+}
+
+void Matrix::printMatrix() const
+{
+    for (const auto &row : matrix)
+    {
+        for (const auto &el : row)
+        {
+            std::cout << el << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+bool Matrix::checkZero(int i, int j)
+{
+    bool flag = true;
+    while (i < n)
+    {
+        if (matrix[i][j].numerator == 0)
+            return false;
+        ++i;
+    }
+    return false;
+}
+
+void Matrix::setMaxElement(int starti, int j)
+{
+    int swapPos = -1;
+    Fraction mmax = matrix[starti][j];
+    for (int i = starti + 1; i < n; ++i)
+    {
+        if (Fraction::abs(matrix[i][j]) > mmax)
+        {
+            mmax = matrix[i][j];
+            swapPos = i;
+        }
+    }
+    if (swapPos != -1)
+    {
+        for (int i = 0; i < m; ++i)
+        {
+            std::swap(matrix[starti][i], matrix[swapPos][i]);
+        }
+    }
+}
+
+bool Matrix::noSolutions()
+{
+    for (int i = n - 1; i > -1; --i)
+    {
+        if (matrix[i][m - 1].numerator != 0)
+        {
+            bool isZero = true;
+            for (int j = 0; j < m - 1; ++j)
+            {
+                if (matrix[i][j].numerator != 0)
+                {
+                    isZero = false;
+                    break;
+                }
+            }
+            if (isZero) return true;
+        }
+    }
+    return false;
+}
+
+void Matrix::JordanGauss()
+{
+    int row = 0;
+    int column = 0;
+
+    std::cout << "Start matrix:" << std::endl;
+    printMatrix();
+    std::cout << std::endl;
+    for (; row < n - 1; ++row, ++column)
+    {
+        while (checkZero(row, column) && column < m - 1)
+        {
+            column++;
+        }
+        if (column == m - 1)
+        {
+            break;
+        }
+        setMaxElement(row, column);
+        std::cout << "Set max element at " << row << " " << column << std::endl;
+        printMatrix();
+        std::cout << std::endl;
+        for (int i = column + 1; i < m; ++i)
+        {
+            matrix[row][i] /= matrix[row][column];
+        }
+        matrix[row][column].numerator = 1;
+        matrix[row][column].denominator = 1;
+        std::cout << "Change line " << row << std::endl;
+        printMatrix();
+        std::cout << std::endl;
+        for (int i = row + 1; i < n; ++i)
+        {
+            for (int j = column + 1; j < m; ++j)
+            {
+                matrix[i][j] -= matrix[row][j] * matrix[i][column];
+            }
+            matrix[i][column].numerator = 0;
+            matrix[i][column].denominator = 1;
+        }
+        for (int i = row - 1; i > -1; --i)
+        {
+            for (int j = column + 1; j < m; ++j)
+            {
+                matrix[i][j] -= matrix[row][j] * matrix[i][column];
+            }
+            matrix[i][column].numerator = 0;
+            matrix[i][column].denominator = 1;
+        }
+        std::cout << "Zeroing a column " << column << std::endl;
+        printMatrix();
+        std::cout << std::endl;
+    }
+    while (matrix[row][column].numerator == 0 && column < m - 1)
+    {
+        column++;
+    }
+    if (column != m - 1)
+    {
+        for (int i = column + 1; i < m; ++i)
+        {
+            matrix[row][i] /= matrix[row][column];
+        }
+        matrix[row][column].numerator = 1;
+        matrix[row][column].denominator = 1;
+        std::cout << "Change line " << row << std::endl;
+        printMatrix();
+        std::cout << std::endl;
+        for (int i = row - 1; i > -1; --i)
+        {
+            for (int j = column + 1; j < m; ++j)
+            {
+                matrix[i][j] -= matrix[row][j] * matrix[i][column];
+            }
+            matrix[i][column].numerator = 0;
+            matrix[i][column].denominator = 1;
+        }
+        std::cout << "Zeroing a column " << column << std::endl;
+        printMatrix();
+        std::cout << std::endl;
+    }
+
+    if (noSolutions()){
+        std::cout << "The system has no solutions" << std::endl;
+        return;
+    }
+    else{
+        std::cout << "Solution:" << std::endl;
+        for(int i = 0, j = 0; i < n && j < m - 1; ++i, ++j){
+            if (matrix[i][j] == Fraction{1}){
+                std::cout << "x" << j+1 << " = " << matrix[i][m-1] << " ";
+                for(int tmp = j+1; tmp < m-1; ++tmp){
+                    if (matrix[i][tmp].numerator != 0){
+                        std::cout << (matrix[i][tmp].numerator < 0 ? "+ " : "- ");
+                        std::cout << Fraction::abs(matrix[i][tmp]) << "(x" << tmp+1 << ") ";
+                    }
+                }
+                std::cout << std::endl;
+            }
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        std::cerr << "./main <filename>" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    std::ifstream in(argv[1]);
+    Matrix matrix(in);
+    matrix.JordanGauss();
+    return 0;
+}
